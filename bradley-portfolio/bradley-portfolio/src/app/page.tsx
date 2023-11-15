31acd0357e3e133c2f2de51e582f066a29bfe76c
Bradley-Hill <brad.michael.hill@gmail.com>
2023-11-15 20:55:15 +0100

    Navbar burger menu written

--- bradley-portfolio/bradley-portfolio/src/app/page.tsx
+++ bradley-portfolio/bradley-portfolio/src/app/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import React from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "@/components/Navbar";
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Testimonials from "@/components/Testimonials";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Navbar />
-    </>
+      <About />
+      <Projects />
+      <Testimonials />
+      <Contact />
+    </div>
   );
 }
