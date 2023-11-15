d188c2399745b4611f18dbf63dab7e60cb703a8e
Bradley-Hill <brad.michael.hill@gmail.com>
2023-11-15 14:03:09 +0100

    reinstalled react-typescript project with tailwind

--- bradley-portfolio/src/App.tsx
+++ bradley-portfolio/src/App.tsx
@@ -1,23 +1,25 @@
-import React from "react";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
-import Contact from "./components/Contact";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-      <Testimonials />
-      <Contact />
-      <p>Blablablabla</p>
-    </main>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
