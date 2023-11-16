b275f0119ae25387a516703fa0953841dbf3e9cf
Bradley-Hill <brad.michael.hill@gmail.com>
2023-11-16 14:59:57 +0100

    added some form validation for teh email input and sticky navbar

--- bradley-portfolio/src/components/Contact.tsx
+++ bradley-portfolio/src/components/Contact.tsx
@@ -1,3 +1,4 @@
+import "../app/styles/Contact.css";
 import { FaTwitter, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import emailjs from "emailjs-com";
 import { useState } from "react";
@@ -7,8 +8,19 @@ export default function Contact() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
+  function validateEmail(email: string) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+
+    if (!validateEmail(email)) {
+      alert("Invalid email address");
+      return;
+    }
+
     emailjs
       .sendForm(
         "service_b6knv4a",
@@ -31,49 +43,80 @@ export default function Contact() {
 
   return (
     <>
-      <div>
-        <a href="https://twitter.com/YOUR_TWITTER_HANDLE">
+      <div
+        className="lg:text-center"
+        style={{ marginBottom: "1rem", textAlign: "center" }}
+      >
+        <h2 className="text-4xl text-gray-600 font-bold tracking-wide uppercase">
+          Socials
+        </h2>
+      </div>
+      <div className="social-media" style={{ marginBottom: "2rem" }}>
+        <a href="https://twitter.com/KingmObYojimbo">
           <FaTwitter />
         </a>
-        <a href="https://github.com/YOUR_GITHUB_HANDLE">
+        <a href="https://github.com/Bradley-Hill">
           <FaGithub />
         </a>
-        <a href="https://www.instagram.com/YOUR_INSTAGRAM_HANDLE/">
+        <a href="https://www.instagram.com/hill.bradley/">
           <FaInstagram />
         </a>
-        <a href="https://www.linkedin.com/in/YOUR_LINKEDIN_HANDLE/">
+        <a href="https://www.linkedin.com/in/bradley-michael-hill/">
           <FaLinkedin />
         </a>
       </div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="user_name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="user_email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <label htmlFor="message">Message:</label>
-        <textarea
-          id="message"
-          name="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          required
-        />
-        <button type="submit">Send</button>
-      </form>
+      <div className="bg-gray-100">
+        <div
+          className="lg:text-center"
+          style={{ marginBottom: "1rem", textAlign: "center" }}
+        >
+          <h2
+            id="contact"
+            className="text-4xl text-gray-600 font-bold tracking-wide uppercase"
+          >
+            Contact Me
+          </h2>
+        </div>
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            maxWidth: "600px",
+            maxHeight: "350px",
+            margin: "0 auto",
+            padding: 0,
+          }}
+        >
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="user_name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="user_email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          />
+          <button className="submit-btn" type="submit">
+            Send
+          </button>
+        </form>
+      </div>
     </>
   );
 }
--- bradley-portfolio/src/components/Navbar.tsx
+++ bradley-portfolio/src/components/Navbar.tsx
@@ -8,7 +8,10 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+    <nav
+      id="Navbar"
+      className="flex items-center justify-between flex-wrap bg-gray-800 p-6"
+    >
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <span className="font-semibold text-xl tracking-tight">
           Bradley M. Hill
